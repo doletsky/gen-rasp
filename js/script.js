9ff12f5708daec3fe9256c9566a1631587d8b42b
doletsky <doletsky@bk.ru>
2016-03-16 20:24:04 +0300

    block tune part4-2

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ var addAll={
 
 var flagWeek=0;//если 5я неделя, то службы распределять
 
-var rasp={}
+var rasp={};
 var weekDey=month.first;
 var numWeek=1;
 
@@ -172,24 +172,69 @@ $(document).ready(function(){
 
     if(window.localStorage.prist){
         //load from localStorage
-        var pristLS=JSON.parse(localStorage.prist);
+        var pristLS=JSON.parse(localStorage.prist);console.log(pristLS);
+        var pristLSRule=JSON.parse(localStorage.rulePrist);
         $('.input_prist_s').html('');//clear list
         var addPrist='';
         for(var key in pristLS){
+            addPrist=pristLS[key];
             if(addPrist.length){
+                var arRuleWday=[];
+                for(var k=1;k<=7;k++){
+                    if(k in pristLSRule[key]['dweek']){
+                        arRuleWday[k]=' '+pristLSRule[key]['dweek'][k];
+                    }else{
+                        if(k==7) arRuleWday[k]=' act';
+                        else arRuleWday[k]='';
+                    }
+                }
                 var cnt=$(".input_prist_s").children("input.person").length;
                 $(".input_prist_s")
                     .append('<input class="person" type="text" value="'+addPrist+'" name="ips'+cnt+'"><span name="ips'+cnt+'" class="del">x</span>' +
-                        ' <span name="ips'+cnt+'" class="dweek" value="1">Пн</span>'+
-                        ' <span name="ips'+cnt+'" class="dweek" value="2">Вт</span>'+
-                        ' <span name="ips'+cnt+'" class="dweek" value="3">Ср</span>'+
-                        ' <span name="ips'+cnt+'" class="dweek" value="4">Чт</span>'+
-                        ' <span name="ips'+cnt+'" class="dweek" value="5">Пт</span>'+
-                        ' <span name="ips'+cnt+'" class="dweek" value="6">Сб</span>'+
-                        ' <span name="ips'+cnt+'" class="dweek act" value="7">Вс</span>'+
+                        ' <span name="ips'+cnt+'" class="dweek'+arRuleWday[1]+'" value="1">Пн</span>'+
+                        ' <span name="ips'+cnt+'" class="dweek'+arRuleWday[2]+'" value="2">Вт</span>'+
+                        ' <span name="ips'+cnt+'" class="dweek'+arRuleWday[3]+'" value="3">Ср</span>'+
+                        ' <span name="ips'+cnt+'" class="dweek'+arRuleWday[4]+'" value="4">Чт</span>'+
+                        ' <span name="ips'+cnt+'" class="dweek'+arRuleWday[5]+'" value="5">Пт</span>'+
+                        ' <span name="ips'+cnt+'" class="dweek'+arRuleWday[6]+'" value="6">Сб</span>'+
+                        ' <span name="ips'+cnt+'" class="dweek'+arRuleWday[7]+'" value="7">Вс</span>'+
                         ' <input class="person-data-act" name="ips'+cnt+'" type="text"> <input class="person-data-novalid" name="ips'+cnt+'" type="text">');
             }
             addPrist=pristLS[key];
+        }
+
+    }
+    if(window.localStorage.diak){
+        //load from localStorage
+        var diakLS=JSON.parse(localStorage.diak);
+        var diakLSRule=JSON.parse(localStorage.ruleDiak);
+        $('.input_prist_d').html('');//clear list
+        var addDiak='';
+        for(var key in diakLS){
+            addDiak=diakLS[key];
+            if(addDiak.length){
+                var arRuleWday=[];
+                for(var k=1;k<=7;k++){
+                    if(k in diakLSRule[key]['dweek']){
+                        arRuleWday[k]=' '+diakLSRule[key]['dweek'][k];
+                    }else{
+                        if(k==7) arRuleWday[k]=' act';
+                        else arRuleWday[k]='';
+                    }
+                }
+                var cnt=$(".input_prist_d").children("input.person").length;
+                $(".input_prist_d")
+                    .append('<input class="person" type="text" value="'+addDiak+'" name="ipd'+cnt+'"><span name="ipd'+cnt+'" class="del">x</span>' +
+                        ' <span name="ipd'+cnt+'" class="dweek'+arRuleWday[1]+'" value="1">Пн</span>'+
+                        ' <span name="ipd'+cnt+'" class="dweek'+arRuleWday[2]+'" value="2">Вт</span>'+
+                        ' <span name="ipd'+cnt+'" class="dweek'+arRuleWday[3]+'" value="3">Ср</span>'+
+                        ' <span name="ipd'+cnt+'" class="dweek'+arRuleWday[4]+'" value="4">Чт</span>'+
+                        ' <span name="ipd'+cnt+'" class="dweek'+arRuleWday[5]+'" value="5">Пт</span>'+
+                        ' <span name="ipd'+cnt+'" class="dweek'+arRuleWday[6]+'" value="6">Сб</span>'+
+                        ' <span name="ipd'+cnt+'" class="dweek'+arRuleWday[7]+'" value="7">Вс</span>'+
+                        ' <input class="person-data-act" name="ipd'+cnt+'" type="text"> <input class="person-data-novalid" name="ipd'+cnt+'" type="text">');
+            }
+            addDiak=diakLS[key];
         }
 
     }
@@ -252,33 +297,98 @@ $(document).ready(function(){
                     }
                 });
                 var strDmonthAct=$('.person-data-act[name="'+nmdw+'"]').val();
-                var arDmonthAct= strDmonthAct.split(',');
-                for(var i in arDmonthAct){
-                    var arLine=arDmonthAct[i].split('-');
-                    if(arLine.length>1){
-                        for(var j=parseInt(arLine[0]);j<=parseInt(arLine[1]);j++){
-                            rulePerson[cntPerson]['dmonth'][j]='act';
+                if(strDmonthAct.length>0){
+                    var arDmonthAct= strDmonthAct.split(',');
+                    for(var i in arDmonthAct){
+                        var arLine=arDmonthAct[i].split('-');
+                        if(arLine.length>1){
+                            for(var j=parseInt(arLine[0]);j<=parseInt(arLine[1]);j++){
+                                rulePerson[cntPerson]['dmonth'][j]='act';
+                            }
+                        }else{
+                            rulePerson[cntPerson]['dmonth'][parseInt(arDmonthAct[i])]='act';
                         }
-                    }else{
-                        rulePerson[cntPerson]['dmonth'][parseInt(arDmonthAct[i])]='act';
                     }
                 }
+
                 var strDmonthnovalid=$('.person-data-novalid[name="'+nmdw+'"]').val();
-                var arDmonthnovalid= strDmonthnovalid.split(',');
-                for(var i in arDmonthnovalid){
-                    var arLine=arDmonthnovalid[i].split('-');
-                    if(arLine.length>1){
-                        for(var j=parseInt(arLine[0]);j<=parseInt(arLine[1]);j++){
-                            rulePerson[cntPerson]['dmonth'][j]='novalid';
+                if(strDmonthnovalid.length>0){
+                    var arDmonthnovalid= strDmonthnovalid.split(',');
+                    for(var i in arDmonthnovalid){
+                        var arLine=arDmonthnovalid[i].split('-');
+                        if(arLine.length>1){
+                            for(var j=parseInt(arLine[0]);j<=parseInt(arLine[1]);j++){
+                                rulePerson[cntPerson]['dmonth'][j]='novalid';
+                            }
+                        }else{
+                            rulePerson[cntPerson]['dmonth'][parseInt(arDmonthnovalid[i])]='novalid';
                         }
-                    }else{
-                        rulePerson[cntPerson]['dmonth'][parseInt(arDmonthnovalid[i])]='novalid';
                     }
                 }
+
                 cntPerson++;
             }
         });
-        console.log(rulePerson);
+        //собираем diakons
+        var cntDiak=1;
+        var diakons={};
+        var ruleDiak={};
+        $('.input_prist_d .person').each(function(){
+            if($(this).val().length>0){
+                //name
+                diakons[cntDiak]=$(this).val();
+                //dweek
+                var nmdw=$(this).attr('name');
+                ruleDiak[cntDiak]={};
+                ruleDiak[cntDiak]['dweek']={};
+                ruleDiak[cntDiak]['dmonth']={};
+                $('.dweek[name="'+nmdw+'"]').each(function(){
+                    var dwVal=$(this).attr('value');
+                    if($(this).hasClass('novalid')){
+                        ruleDiak[cntDiak]['dweek'][dwVal]= "novalid";
+                    }
+                    if($(this).hasClass('act')){
+                        ruleDiak[cntDiak]['dweek'][dwVal]= "act";
+                    }
+                });
+                var strDmonthAct=$('.person-data-act[name="'+nmdw+'"]').val();
+                if(strDmonthAct.length>0){
+                    var arDmonthAct= strDmonthAct.split(',');
+                    for(var i in arDmonthAct){
+                        var arLine=arDmonthAct[i].split('-');
+                        if(arLine.length>1){
+                            for(var j=parseInt(arLine[0]);j<=parseInt(arLine[1]);j++){
+                                ruleDiak[cntDiak]['dmonth'][j]='act';
+                            }
+                        }else{
+                            ruleDiak[cntDiak]['dmonth'][parseInt(arDmonthAct[i])]='act';
+                        }
+                    }
+                }
+
+                var strDmonthnovalid=$('.person-data-novalid[name="'+nmdw+'"]').val();
+                if(strDmonthnovalid.length>0){
+                    var arDmonthnovalid= strDmonthnovalid.split(',');
+                    for(var i in arDmonthnovalid){
+                        var arLine=arDmonthnovalid[i].split('-');
+                        if(arLine.length>1){
+                            for(var j=parseInt(arLine[0]);j<=parseInt(arLine[1]);j++){
+                                ruleDiak[cntDiak]['dmonth'][j]='novalid';
+                            }
+                        }else{
+                            ruleDiak[cntDiak]['dmonth'][parseInt(arDmonthnovalid[i])]='novalid';
+                        }
+                    }
+                }
+
+                cntDiak++;
+            }
+        });
+//        console.log(ruleDiak);
+        localStorage.prist=JSON.stringify(persons);
+        localStorage.diak=JSON.stringify(diakons);
+        localStorage.rulePrist=JSON.stringify(rulePerson);
+        localStorage.ruleDiak=JSON.stringify(ruleDiak);
     });
 //    var dt=1;
 //    var hh=1;
